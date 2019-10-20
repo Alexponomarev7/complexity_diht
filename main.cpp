20bbf312220d2ea0ea3bedd529ca9dae5e93890f
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2019-10-21 01:39:48 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "tcp/testlib.h"
+
+int main() {
+
+  return 0;
+}
---
+++ tcp/greedy.h
@@ -0,0 +1,24 @@
+//
+// Created by Алексей Пономарев on 2019-10-21.
+//
+
+#ifndef COMPLEXITY_DIHT_GREEDY_H
+#define COMPLEXITY_DIHT_GREEDY_H
+
+
+class Greedy {
+public:
+  Greedy() {
+
+  }
+
+  void generate() {
+
+  }
+
+private:
+  int min_x, min_y, max_x, max_y;
+};
+
+
+#endif //COMPLEXITY_DIHT_GREEDY_H
