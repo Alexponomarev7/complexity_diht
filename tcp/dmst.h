f24646a18affea4348daae78f010adf09d882077
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2019-10-21 03:34:07 +0300

    add dmst flower graph

--- tcp/dmst.h
+++ tcp/dmst.h
@@ -1,8 +1,56 @@
-//
-// Created by Алексей Пономарев on 2019-10-21.
-//
+#pragma once
 
-#ifndef COMPLEXITY_DIHT_DMST_H
-#define COMPLEXITY_DIHT_DMST_H
+#include <string>
+#include <vector>
 
-#endif //COMPLEXITY_DIHT_DMST_H
+#include "../common/testcase.h"
+#include "../common/test.h"
+#include "../common/testsuite.h"
+
+#include "closure.h"
+
+#include "testlib.h"
+
+namespace tcp {
+
+namespace dmst {
+
+Test GenerateFlowerGr(const TestCase<std::string, int>& test_case) {
+  Test result;
+  int N = test_case.Get("N");
+  int max_edge = test_case.Get("max_edge");
+  int head = rnd.next(N);
+  result << N << "\n";
+  std::vector<std::vector<int>> matrix(N, std::vector<int>(N, 0));
+  for (int i = 0; i < N; ++i) {
+    if (i != head) {
+      matrix[head][i] = matrix[i][head] = rnd.next(max_edge) + 1;
+    }
+  }
+  DoTakeGraphClosure(matrix);
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      result << matrix[i][j] << ' ';
+      if (j == N - 1) {
+        result << '\n';
+      }
+    }
+  }
+  return result;
+}
+
+void gen() {
+  TestSuite suite;
+
+  TestCase<std::string, int> sample_case;
+  sample_case.Set("N", 50);
+  sample_case.Set("max_edge", 1000);
+  suite.add(GenerateFlowerGr(sample_case));
+
+  suite.save();
+}
+
+}
+
+}
